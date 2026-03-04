449bbc11271c5d7988622dc129902567a0b145e1
mahatolalit <lalitmahato20051024@gmail.com>
2026-03-04 12:05:39 +0530

    feat: implement CPU class for instruction execution and memory management

---
+++ cpu.py
@@ -0,0 +1,63 @@
+from alu import add_ternary, subtract
+from conversion import to_balanced_ternary
+
+
+class CPU:
+
+    def __init__(self, registers, memory):
+        self.registers = registers
+        self.memory = memory
+
+    def execute(self, instruction):
+
+        op = instruction[0]
+
+        if op == "MOV":
+            reg = instruction[1]
+            value = instruction[2]
+
+            self.registers.write(reg, to_balanced_ternary(value))
+
+
+        elif op == "ADD":
+            r1 = instruction[1]
+            r2 = instruction[2]
+
+            a = self.registers.read(r1)
+            b = self.registers.read(r2)
+
+            result = add_ternary(a, b)
+
+            self.registers.write(r1, result)
+
+
+        elif op == "SUB":
+            r1 = instruction[1]
+            r2 = instruction[2]
+
+            a = self.registers.read(r1)
+            b = self.registers.read(r2)
+
+            result = subtract(a, b)
+
+            self.registers.write(r1, result)
+
+
+        elif op == "LOAD":
+
+            reg = instruction[1]
+            addr = instruction[2]
+
+            value = self.memory.read(addr)
+
+            self.registers.write(reg, value)
+
+
+        elif op == "STORE":
+
+            reg = instruction[1]
+            addr = instruction[2]
+
+            value = self.registers.read(reg)
+
+            self.memory.write(addr, value)
