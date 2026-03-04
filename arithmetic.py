226b106d37ba3fad5ac7da7a7f0f74262a04e267
mahatolalit <lalitmahato20051024@gmail.com>
2026-03-04 12:04:46 +0530

    feat: add functions for trit and ternary addition

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+def add_trit(a, b, carry=0):
+    s = a + b + carry
+
+    if s > 1:
+        return s - 3, 1
+
+    if s < -1:
+        return s + 3, -1
+
+    return s, 0
+
+def add_ternary(a, b):
+    
+    # make both numbers same length
+    max_len = max(len(a), len(b))
+    
+    a = [0]*(max_len - len(a)) + a
+    b = [0]*(max_len - len(b)) + b
+
+    result = []
+    carry = 0
+
+    # start from rightmost trit
+    for i in range(max_len-1, -1, -1):
+        
+        digit, carry = add_trit(a[i], b[i], carry)
+        result.insert(0, digit)
+
+    if carry != 0:
+        result.insert(0, carry)
+
+    return result
